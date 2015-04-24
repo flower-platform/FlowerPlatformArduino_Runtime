7dd1cd4c9639322d73163b8479d21c40a3b8c2cb
Claudiu Matei <claudiu@shmessenger.org>
2015-04-24 14:11:24 +0300

    GH356 - Updated Input and Output

--- Input.h
+++ Input.h
@@ -6,16 +6,15 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+
 class Input : public Component, public EventDispatcher {
 protected:
 	int lastValue = -1;
 
 public:
 	static int VALUE_CHANGED_EVENT;
-	static int EVENT_TYPE_VALUE_ON;
-	static int EVENT_TYPE_VALUE_OFF;
 
-	Input() : EventDispatcher(3, VALUE_CHANGED_EVENT) { }
+	Input() : EventDispatcher(1, VALUE_CHANGED_EVENT) { }
 
 	uint8_t pin;
 	bool isAnalog = false;
@@ -47,24 +46,10 @@ public:
 
 		lastValue = value;
 
-		if (isAnalog) {
-			return;
-		}
-
-		if (value == HIGH) {
-			event.type = EVENT_TYPE_VALUE_ON;
-		} else {
-			event.type = EVENT_TYPE_VALUE_OFF;
-		}
-		dispatchEvent(&event);
-
-
 	}
 
 };
 
 int Input::VALUE_CHANGED_EVENT = newEventType();
-int Input::EVENT_TYPE_VALUE_ON = newEventType();
-int Input::EVENT_TYPE_VALUE_OFF = newEventType();
 
 #endif
